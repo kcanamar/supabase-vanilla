93392cdd7b7ec6fdd22a841c2bc91b6a86013f8c
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-03-24 18:52:47 -0700

    auth UI updates for signIn and signOut

--- public/app.js
+++ public/app.js
@@ -2,14 +2,21 @@
 
 const { createClient } = supabase;
 
-const supaUrl = ''
-const supaAnon = ''
 
 const supaClient = createClient(supaUrl, supaAnon)
 
 // Html Elements
 
 const loginButton = document.querySelector("#signInBtn")
+const logoutButton = document.querySelector("#signOutBtn")
+const whenSignedOut= document.querySelector("#whenSignedOut")
+const whenSignedIn = document.querySelector("#whenSignedIn")
+const userDetails = document.querySelector("#userDetails")
+const allThingsSection = document.querySelector("#allThings")
+const allThingsList = document.querySelector("#allThingsList")
+const myThingsSection = document.querySelector("#theThings")
+const myThingsList = document.querySelector("#thingsList")
+const createThing = document.querySelector("#createThing")
 
 // Event Listeners
 
@@ -18,3 +25,49 @@ loginButton.addEventListener("click", () => {
         provider: 'google',
     });
 })
+
+logoutButton.addEventListener("click", () => {
+    supaClient.auth.signOut()
+})
+
+// Init
+
+checkUserOnStartUp()
+
+supaClient.auth.onAuthStateChange((_event, session) => {
+    if (session?.user) {
+        adjustForUser(session.user)
+    } else {
+        adjustForNoUser()
+    }
+})
+
+// function declarations
+
+async function checkUserOnStartUp() {
+    const {
+        data: { user },
+    } = await supaClient.auth.getUser();
+    if (user) {
+        adjustForUser(user)
+    } else {
+        adjustForNoUser()
+    }
+}
+
+function adjustForUser(user) {
+    whenSignedIn.hidden = false;
+    whenSignedOut.hidden = true;
+    myThingsSection.hidden = false;
+    userDetails.innerHTML = `
+    <h3> Hello ${user.user_metadata.full_name}</h3>
+    <img src="${user.user_metadata.avatar_url}" />
+    <p>UID: ${user.id}</p>
+    `
+}
+
+function adjustForNoUser() {
+    whenSignedIn.hidden = true;
+    whenSignedOut.hidden = false;
+    myThingsSection.hidden = true;
+}
